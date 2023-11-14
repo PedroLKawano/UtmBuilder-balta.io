26d1e2b5ab1dbe71637b50b40aaf5e467f29f7b9
Pedro Lucas Kawano <pedrolucas_kawano@hotmail.com>
2023-11-14 16:14:23 -0300

    commit

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -5,10 +5,5 @@ namespace UtmBuilder.Core;
 public class Utm
 {
     public Url Url { get; set; }
-    public string Source { get; set; }
-    public string Medium { get; set; }
-    public string Name { get; set; }
-    public string Id { get; set; }
-    public string Term { get; set; }
-    public string Content { get; set; }
+    public Campaign Campaign { get; set; }
 }
