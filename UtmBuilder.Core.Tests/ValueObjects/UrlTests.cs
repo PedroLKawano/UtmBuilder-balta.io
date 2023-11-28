51500a4ac71d71987f8660375b60da4c8655b208
Pedro Lucas Kawano <pedro.lucas@inovafarma.com.br>
2023-11-28 09:26:23 -0300

    commit

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -1,11 +1,28 @@
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
 namespace UtmBuilder.Core.Tests.ValueObjects;
 
 [TestClass]
 public class UrlTests
 {
     [TestMethod]
-    public void X()
+    public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
+        try
+        {
+            var url = new Url(address: "banana");
+            Assert.Fail();
+        }
+        catch (InvalidUrlException)
+        {
+            Assert.IsTrue(true);
+        }
+    }
 
+    [TestMethod]
+    public void ShouldNotReturnExceptionWhenUrlIsValid()
+    {
+        Assert.Fail();
     }
 }
