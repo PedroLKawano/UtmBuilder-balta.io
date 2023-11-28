a37f4cb967264e010748db549b169fcfbf3f1408
Pedro Lucas Kawano <pedro.lucas@inovafarma.com.br>
2023-11-28 11:12:18 -0300

    commit

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -6,17 +6,48 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 [TestClass]
 public class UrlTests
 {
+    private const string InvalidUrl = "banana";
+    private const string ValidUrl = "https://balta.io";
+
     [TestMethod]
     [ExpectedException(typeof(InvalidUrlException))]
     public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
-        new Url(address: "banana");
+        new Url(address: InvalidUrl);
     }
 
     [TestMethod]
     public void ShouldNotReturnExceptionWhenUrlIsValid()
     {
-        new Url("https://balta.io");
+        new Url(ValidUrl);
         Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    [DataRow(data1: " ", moreData: true)]
+    [DataRow(data1: "http", moreData: true)]
+    [DataRow(data1: "banana", moreData: true)]
+    [DataRow(data1: "https://balta.io", moreData: false)]
+    public void TestUrl(
+        string link,
+        bool expectException)
+    {
+        if (expectException)
+        {
+            try
+            {
+                new Url(link);
+                Assert.Fail();
+            }
+            catch (InvalidUrlException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        else
+        {
+            new Url(link);
+            Assert.IsTrue(true);
+        }
     }
 }
