65d6216c785a7a5105bea74645159bdee6608160
Pedro Lucas Kawano <pedro.lucas@inovafarma.com.br>
2023-11-29 15:29:30 -0300

    commit

--- UtmBuilder.Core.Tests/UtmTests.cs
+++ UtmBuilder.Core.Tests/UtmTests.cs
@@ -5,11 +5,15 @@ namespace UtmBuilder.Core.Tests;
 [TestClass]
 public class UtmTests
 {
-    [TestMethod]
-    public void ShouldReturnUrlFromUtm()
-    {
-        var url = new Url("https://balta.io/");
-        var campaign = new Campaign(
+    private const string Result = "https://balta.io/" +
+                                  "?utm_source=src" +
+                                  "&utm_medium=med" +
+                                  "&utm_campaign=nme" +
+                                  "&utm_id=id" +
+                                  "&utm_term=ter" +
+                                  "&utm_content=ctn";
+    private readonly Url _url = new(address: "https://balta.io/");
+    private readonly Campaign _campaign = new(
             source: "src",
             medium: "med",
             name: "nme",
@@ -17,15 +21,25 @@ public class UtmTests
             term: "ter",
             content: "ctn");
 
-        var utm = new Utm(url, campaign);
-        var result = "https://balta.io/" +
-                    "?utm_source=src" +
-                    "&utm_medium=med" +
-                    "&utm_campaign=nme" +
-                    "&utm_id=id" +
-                    "&utm_term=ter" +
-                    "&utm_content=ctn";
+    [TestMethod]
+    public void ShouldReturnUrlFromUtm()
+    {
+        var utm = new Utm(_url, _campaign);
 
-        Assert.AreEqual(expected: result, actual: utm.ToString());
+        Assert.AreEqual(expected: Result, actual: utm.ToString());
+        Assert.AreEqual(expected: Result, actual: (string)utm);
+    }
+
+    [TestMethod]
+    public void ShouldReturnUtmFromUrl()
+    {
+        Utm utm = Result;
+        Assert.AreEqual(expected: "https://balta.io/", actual: utm.Url.Address);
+        Assert.AreEqual(expected: "src", actual: utm.Campaign.Source);
+        Assert.AreEqual(expected: "med", actual: utm.Campaign.Medium);
+        Assert.AreEqual(expected: "nme", actual: utm.Campaign.Name);
+        Assert.AreEqual(expected: "id", actual: utm.Campaign.Id);
+        Assert.AreEqual(expected: "ter", actual: utm.Campaign.Term);
+        Assert.AreEqual(expected: "ctn", actual: utm.Campaign.Content);
     }
 }
